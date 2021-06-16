cbaccca693699910ad470f42d70b27c0f2f16655
Alex <alex@monney.me>
2021-06-16 21:42:07 +0100

    move css to scss files and add sass compiler

--- bin/build.js
+++ bin/build.js
@@ -1,7 +1,10 @@
 #!/usr/bin/env node
 "use strict";
 
+const fs = require('fs/promises')
 const path = require('path');
+const sass = require('sass');
+
 const appRoot = path.resolve(__dirname, '..');
 const config = require(path.resolve(appRoot, 'config.json'));
 
@@ -10,4 +13,21 @@ const Handlebars = require('HandlebarsExtended')({
   ...config.paths
 });
 
+
+// build html files with Handlebard
 Handlebars.buildSite(config)
+
+// build css files from SASS
+const files = ['main', 'normalize'];
+files.forEach( async file => {
+  const infile = path.resolve(appRoot, 'src', 'sass', `${file}.scss`);
+  const outfile = path.resolve(appRoot, 'docs', 'css', `${file}.css`);
+  try {
+    const result = sass.renderSync( { file: infile, outFile: outfile } );
+    await fs.writeFile(outfile, result.css)
+  }
+  catch (e){
+    console.log(`failed to compile ${infile} : ${e}`)
+  }
+})
+  
