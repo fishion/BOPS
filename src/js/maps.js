04c5dcb0796f09b4c9314e36b925a2384a707435
Alex <alex@monney.me>
2021-06-27 23:35:09 +0100

    brighton-centred openlayer map and one marker

--- src/js/maps.js
+++ src/js/maps.js
@@ -1,17 +1,43 @@
 import 'ol/ol.css';
 import {Map, View} from 'ol';
+// Layers are map layers that can contain things
 import TileLayer from 'ol/layer/Tile';
-import OSM from 'ol/source/OSM';
+import VectorLayer from 'ol/layer/Vector';
+// Sources are sources of gfx for display in layers
+import OSMSource from 'ol/source/OSM';
+import VectorSource from 'ol/source/Vector';
+// Features - A vector object for geographic features with a geometry and other attribute
+import Feature from 'ol/Feature';
+// method for getting a projection from latlong
+import {fromLonLat} from 'ol/proj';
+// a geometry point object
+import Point from 'ol/geom/Point';
+// 
+import {Style, Icon} from 'ol/style';
+
+const feature = new Feature({
+  geometry: new Point(fromLonLat([-0.2005,50.8515]))
+})
+const vecsource = new VectorSource()
+vecsource.addFeature(feature);
 
 const map = new Map({
   target: 'map',
   layers: [
     new TileLayer({
-      source: new OSM()
+      source: new OSMSource()
+    }),
+    new VectorLayer({
+      source: vecsource,
+      style: new Style({
+        image: new Icon({
+          src: '../img/map-marker.png'
+        })
+      }) 
     })
   ],
   view: new View({
-    center: [0, 0],
-    zoom: 0
+    center: fromLonLat([-0.195,50.842487]),
+    zoom: 13
   })
 });
